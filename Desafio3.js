2f3aa17f21295e34f448b0145a2813616aae55fd
Martina Rodriguez Arona <tini.arona16@gmail.com>
2024-10-04 17:42:19 -1200

    le agregue unos detalles

--- Desafio3.js
+++ Desafio3.js
@@ -4,14 +4,12 @@ const precioUnitario = 3684920
 
 let E = parseInt(prompt('ingrese la cantidad deseada: '));
 
-let y = alert("Costo total de su compra: $"+precioUnitario*E )
-
 let PrecioSinDescuento = precioUnitario * E
 
 let PrecioConDescuento = PrecioSinDescuento - (PrecioSinDescuento * 10/100) 
 
 if(E >= 5) {
-    alert("costo total: $"+ PrecioConDescuento)
+    alert(`costo total con un descuento del 10%:  \n  ${"$" + PrecioConDescuento}`)
 } else{
-    alert("Costo total de su compra: $"+PrecioSinDescuento )
+    alert(`Costo total de su compra:  \n ${"$" + PrecioSinDescuento}` ) 
 }
